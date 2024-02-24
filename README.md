8a24a41ecd72631c150dc95e0a771183c616a16b
Kigrok <kiribel6@gmail.com>
2024-02-24 20:24:19 +0300

    0.2

--- README.md
+++ README.md
@@ -1,44 +1,47 @@
 # Pmap
-
-![Pmap Logo](https://lh3.googleusercontent.com/drive-viewer/AJc5JmTJMZHWfj51bhxa9Oib1P_uIcBEcsuycMmX-K-Kaxm3rLNehMjLX6ODkUaL0GCa3-TRkdIpEPw=w1366-h656)
-
+[![python](https://img.shields.io/badge/Python-3.11-3776AB.svg?&logo=python&logoColor=python)](https://www.python.org) [![aiohttp](https://img.shields.io/static/v1?label=aiohttp&message=v3.9.3&logo=Aiohttp&colorColor=Aiohttp)](https://docs.aiohttp.org/en/stable/) [![License](https://img.shields.io/badge/License-MIT-black.svg)]() 
+![Pmap Logo](https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYSxgjU5_MP9MnYgzjNBQU6dekdoSuOAkES4jJ0Jh1iNNRjrkMZxojy0XibLAlggVpyMc0CQGeaxLp_wIlurqRINf6Vm=w1366-h659)
+Pmap is a port scanner that allows a user to check for open ports on a specified website. Using asynchronous and multiprocessing approaches, the program scans the specified website for available ports and returns information about the status and contents of each open port. It also provides additional details about the server, including country, city, and time zone, using the IP address geolocation API.
+---
 ## Multi-threaded port scanner
 
-
-- Port check and information from it
+- Port scanning and port information
 - Selecting the number of threads
-- MSelecting a range of ports to scan
-
-![Pmap](https://lh3.googleusercontent.com/drive-viewer/AJc5JmTF3C5CTaiUixrIiLJjjltau2pAqpwu3rkdwoO_qKF1SDYA2jkcFQ0VXTDanNv00_Rq1cQQCKc=w1366-h612)
-
----
-
-## Help
-
-![Help](https://lh3.googleusercontent.com/drive-viewer/AJc5JmRRLhwSYd4TBjhiSjxI76pmX36-F8A4J7YxW5RQd0wqJp7MNOCJkMZh97DDXpKc42zOwMitE8o=w1366-h656)
+- Select the range of ports to scan
+- Getting information about an open port
 
 ---
-
 ## Install 
-
 Cloning a repository
 ```sh
 git clone https://github.com/Kigrok/Pmap.git
 cd Pmap
 ```
-
 Installing the virtual environment and packages
-
 ```sh
 python3 -m venv venv
 source ./venv/bin/activate
 pip3 install -r requirements.txt
 ```
-
 ---
-
 ## Start
-
 ```sh
-python3 pmap.py  -u [ URL]  -p [ PORT] -t [ THREADS]
+python3 pmap.py [-h] [-p PORTS] [-t THREADS] url
 ```
+---
+## Help
+```
+positional arguments:
+  url                   URL of the website to scan
+
+options:
+  -h, --help            show this help message and exit
+  -p PORTS, --ports PORTS
+                        Number of ports to scan (default: 65535)
+  -t THREADS, --threads THREADS
+                        Number of threads for scanning (default: 500)
+
+```
+---
+## License
+[MIT](LICENSE) Copyright 2023 Venera, Inc.
--- pmap.py
+++ pmap.py
@@ -1,132 +1,146 @@
-import socket, requests, json, argparse, multiprocessing
+#!/usr/bin/env python3
+
+from socket import gethostbyname, socket, AF_INET, SOCK_STREAM
+from argparse import ArgumentParser, Namespace
+from aiohttp import ClientSession
+from multiprocessing import Pool
 from datetime import datetime
 from pyfiglet import Figlet
+from asyncio import run
+import json
 
-def get_args():
+def get_args() -> Namespace:
+    """
+    Retrieve command-line arguments.
+    Returns:
+        argparse.Namespace: A namespace containing command-line arguments.
+    """
+    parser: ArgumentParser = ArgumentParser(description='Pmap ~ Async Port Scanner')
+    parser.add_argument('url', type=str, help='URL of the website to scan')
+    parser.add_argument('-p', '--ports', type=int, default=65535, help='Number of ports to scan (default: 65535)')
+    parser.add_argument('-t', '--threads', type=int, default=500, help='Number of threads for scanning (default: 500)')
+    return parser.parse_args()
 
-	parser = argparse.ArgumentParser()
+async def fetch_url(url: str) -> ClientSession:
+    """
+    Fetches the content of a URL asynchronously.
+    Args:
+        url (str): The URL to fetch.
+    Returns:
+        aiohttp.ClientResponse: The response object containing the fetched content.
+    """
+    async with ClientSession() as session:
+        async with session.get(str(url)) as response:
+            return response
 
-	parser.add_argument('-u', '--url', 
-		dest = 'url', 
-		help = 'URL / IP')
+async def geo_info(ip: str) -> str | Exception:
+    """
+    Retrieves geographical information for a given IP address using an external API.
+    Args:
+        ip (str): The IP address for which to retrieve information.
+    Returns:
+        str: A formatted string containing the geographical information.
+        Exception: An exception raised if the retrieval process fails.
+    """
+    try:
+        async with ClientSession() as session:
+            async with session.get(f'http://ip-api.com/json/{ip}') as response:
+                data: json = await response.json()
+                return f'''Country: \033[34m{data['country']} | {data['countryCode']}\033[00m\n\
+Sity: \033[34m{data['city']}\033[00m\n\
+TimeZone: \033[34m{data['timezone']}\033[00m'''
+    except Exception as e: 
+        raise e
 
-	parser.add_argument('-p', '--port', 
-		dest = 'port', 
-		help = 'Ports range.\nFormat: 8080 or (80-8080) or (80/8080) or (21,80,443,...)')
+async def success(response: ClientSession, ip: str) -> str:
+    """
+    Formats and constructs a success message containing information about the response and IP address.
+    Args:
+        response (ClientSession): The response object from the website.
+        ip (str): The IP address of the website.
+    Returns:
+        str: A formatted success message containing details about the response, IP address, server, and timestamp.
+    """
+    return(f'''{'-'*40}\n{response.host} | \033[36m{ip}\033[00m (\033[32m{response.status}\033[00m)
+Star Scan: \033[36m{(datetime.now()).strftime("%d-%m-%Y %H:%M:%S")}\033[00m
+Server: \033[36m{response.headers.get("Server")}\033[00m
+X-Powered-By: \033[36m{response.headers.get("X-Powered-By")}\033[00m
+{'*'*30}\n{str(await geo_info(ip=str(ip)))}\n{'-'*40}''')
 
-	parser.add_argument('-t', '--threads', 
-		dest = 'threads', 
-		help = 'Number of threads')
+async def get_website_info(url: str, ports: int, threads: int) -> None | Exception:
+    """
+    Retrieves information about a website, such as its IP address, and initiates port scanning.
+    Args:
+        url (str): The URL of the website to scan.
+        ports (int): The number of ports to scan.
+        threads (int): The number of threads for scanning.
+    Raises:
+        Exception: If an error occurs during the process.
+    """
+    try:
+        response: ClientSession = await fetch_url(url=str(url))
+        ip: str = gethostbyname(response.host)
+        print(await success(response=response, ip=str(ip)))
+        await proccessing(ip=str(ip), ports=list(range(ports)), threads=int(threads))
+    except Exception as e:
+        raise e
 
-	options = parser.parse_args()
-	return options
+async def proccessing(ip: str, ports: list[int], threads: int) -> None:
+    """
+    Initiates port scanning for the specified IP address and ports using multiprocessing.
+    Args:
+        ip (str): The IP address to scan.
+        ports (list[int]): A list of port numbers to scan.
+        threads (int): The number of threads for scanning.
+    Raises:
+        OSError: If an error occurs during multiprocessing.
+    """
+    try:
+        with Pool(processes=int(threads)) as process:
+            process.starmap(scan_port, [(ip, port) for port in ports])
+    except OSError as e:
+        print(f'\033[31m{e}\033[00m') 
 
+def port_info(port: int, data: bytes) -> str:
+    """
+    Analyzes the received data and generates a string representing port information.
+    Args:
+        port (int): The port number.
+        data (bytes): The data received from the port.
+    Returns:
+        str: A formatted string containing port information.
+    """
+    data: bytes = data.strip().decode('utf-8')
+    if 'html' in (data).lower():
+        data: str = '<HTML>'
+    return(f'''[\033[31m+\033[00m] Port: \033[95m{port}\033[00m ~ \033[93m{data}\033[00m''')
 
-# Get website's location info
-def info(ip):
+def scan_port(ip: str, port: int) -> None:
+    """
+    Scans a specific port on the given IP address and prints the port information.
+    Args:
+        ip (str): The IP address to scan.
+        port (int): The port number to scan.
+    """
+    with socket(AF_INET, SOCK_STREAM) as sock:
+        sock.settimeout(3)
+        try:
+            sock.connect((ip, port))
+            sock.send(b'GET / HTTP/1.1\r\n\r\n')
+            try:
+                print(port_info(port=int(port), data=bytes(sock.recv(8192))))
+            except: 
+                print(port_info(port=int(port), data=bytes(sock.recv(4096))))
+        except: pass
 
-	''' Output website's country, sity and timezone '''
+async def main():
+    """
+    Main entry point of the program. It prints the ASCII art title of the program,
+    gets command-line arguments using get_args function, and then calls the get_website_info
+    function with the provided URL, ports, and threads.
+    """
+    print(f'\033[31m{Figlet(font="slant").renderText("Pmap")}\033[00m')
+    await get_website_info(url=str(get_args().url), ports=int(get_args().ports), threads=int(get_args().threads))
 
-	try:
-		response = requests.get(f'http://ip-api.com/json/{ip}').json()
-		return response['country'], response['city'], response['timezone'], response['countryCode']
-
-	except Exception as e:
-		return '\033[31mConnection Error\033[00m', '\033[31mN/A\033[00m', '\033[31mN/A\033[00m', '\033[31mN/A\033[00m'
-
-
-# Banner
-def banner(name, ip):
-
-	''' Output banner and ip's info '''
-
-	baner = Figlet(font='slant')
-	print(f'\033[31m{baner.renderText("Pmap")}\033[00m')
-	print('\t Made by \033[32mhttps://github.com/Kigrok\033[00m')
-	print('\n' + ('-' * 30))
-	print(f'Name: \033[36mhttp://{name.strip()}\033[00m')
-	print(f'IP: \033[36m{ip}\033[00m')
-	print(f'''Country: \033[34m{info(ip = ip)[0]} | {info(ip = ip)[3]}\033[00m\n\
-Sity: \033[34m{info(ip = ip)[1]}\033[00m\n\
-TimeZone: \033[34m{info(ip = ip)[2]}\033[00m''')
-	print(f'Scanning start: \033[36m{str(datetime.now().time().strftime("%H:%M:%S"))}\033[00m')
-	print(('-' * 30) + '\n')
-
-
-def scan_port(port):
-
-	''' Output open port and info '''
-
-	sock = socket.socket()
-	sock.settimeout(3)
-	try:
-		sock.connect((ip, int(port)))
-		try:
-			print(f'''[\033[31m+\033[00m] \
-Port: \033[95m{port}\033[00m \
-~ \033[93m{sock.recv(2048).strip().decode("ascii")}\033[00m''')
-		except :
-			try:
-				print(f'''[\033[31m+\033[00m] \
-Port: \033[95m{port}\033[00m \
-~ \033[93m{sock.recv(1024)}\033[00m''')
-			except:
-				try:
-					print(f'''[\033[31m+\033[00m] \
-Port: \033[95m{port}\033[00m \
-~ \033[93m{requests.get(f'http://{ip}:{port}', timeout = 5)}\033[00m''')
-				except:
-					print(f'''[\033[31m+\033[00m] \
-Port: \033[95m{port}\033[00m''')
-
-	except: pass
-	sock.close()
-
-
-def processing(options = None, threads = int(multiprocessing.cpu_count())):
-
-	try:
-		with multiprocessing.Pool(int(threads)) as process:
-
-			if options == None:
-
-				process.map(scan_port, range(65535))
-
-			else:
-
-				if '-' in options:
-					process.map(scan_port, range(int(options.split('-')[0]), (int(options.split('-')[-1])+1)))
-
-				elif '/' in options:
-					process.map(scan_port, range(int(options.split('/')[0]), (int(options.split('/')[-1])+1)))
-
-				elif ',' in options:
-					process.map(scan_port, options.split(','))
-
-				else:
-					process.map(scan_port, range((0), (int(options.split()[0].strip())+1)))
-
-	except KeyboardInterrupt: pass
-
-
-def main():
-
-	start = datetime.now()
-	global ip
-
-	try:
-		ip = socket.gethostbyname(get_args().url)
-		banner(name = get_args().url, ip = ip)
-		processing(
-			options = get_args().port, 
-			threads = get_args().threads)
-		print('\n' + ('-' * 30))
-		print(f'Time: \033[36m{datetime.now() - start}\033[00m')
-
-	except Exception as e:
-		print(f'\033[31mConnection Error: {e}\033[00m')
-
-
-
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    run(main())
